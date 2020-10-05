0ac7768a089a4a7f35049c376502ec2e8123ac2d
Penn <penn11.liu@gmail.com>
2020-10-04 19:46:13 -0700

    [data structure]list

---
+++ list/list.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdio.h>
+
+// gcc -o -g list list.c
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+bool list_add_node(struct node** h, struct node* n)
+{
+    assert(h != NULL);
+    assert(n != NULL);
+    n->next = (*h);
+    (*h) = n;
+    return true;
+}
+
+void list_print(struct node* h)
+{
+    while (h != NULL)
+    {
+        printf("->%d", h->data);
+        h = h->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    //create list
+    //add a node from head
+    //delete a node  from head
+    struct node* head = NULL;
+    struct node* n = (struct node*)malloc(sizeof(struct node));
+    //memset(n, 0, sizeof(struct node));
+    n->data = 0;
+    n->next = NULL;
+    list_add_node(&head, n);
+    n = (struct node*)malloc(sizeof(struct node));
+    n->data = 1;
+    n->next = NULL;
+    list_add_node(&head, n);
+    list_print(head);
+    return 0;
+}
